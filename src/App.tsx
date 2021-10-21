9bf57eedb81795ccd6d63ee7048fe82d5435f6c7
Emmi Siitonen <emmi.siitonen@futurice.com>
2021-10-21 18:05:09 +0300

    List all the countries and allow searching them by name

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { get } from "./api"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Country {
+  name: string
+  population: number
+  region: string
+  // subregion: string
+  capital: string
+
+  flags: {
+    svg: string
+    png: string
+  }
 }
 
-export default App;
+type State = {
+  countries: Country[]
+  search: {
+    input: string
+    typing: boolean
+    timeout: any //TODO:
+  }
+}
+export default class App extends Component {
+  state: State = {
+    countries: [],
+
+    search: {
+      input: "",
+      typing: false,
+      timeout: 0
+    }
+  }
+
+  componentDidMount() {
+    this.getCountries("/all")
+  }
+
+  async getCountries(path: string) {
+    const response = await get<Country[]>(path)
+
+    const countries = response
+      ? response.map(r => {
+          const { name, population, region, capital, flags } = r
+
+          return {
+            name,
+            population,
+            region,
+            capital,
+            flags
+          }
+        })
+      : []
+
+    this.setState({ countries })
+  }
+
+  onChange(e: any) {
+    e.preventDefault()
+
+    if (this.state.search.timeout) {
+      clearTimeout(this.state.search.timeout)
+    }
+
+    this.setState({
+      search: {
+        timeout: setTimeout(
+          () => this.getCountries(`/name/${e.target.value}`),
+          1000
+        )
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <div className="header">
+          <h1 className="title">Where in the world?</h1>
+        </div>
+
+        <div className="toolbar">
+          <input
+            type="text"
+            className="search"
+            placeholder="Search for a country"
+            onChange={this.onChange.bind(this)}
+          />
+          <button className="dropdown">Filter by region </button>
+        </div>
+
+        <div className="list">
+          {this.state.countries.map(country => {
+            const { name, population, region, capital, flags } = country
+
+            return (
+              <div className="country-item" key={country.name}>
+                <img
+                  className="country-flag"
+                  src={flags.svg}
+                  alt={`Flag of ${name}`}
+                />
+                <div className="country-info">
+                  <h2 className="country-name">{name}</h2>
+                  {this.property("Population", population)}
+                  {this.property("Region", region)}
+                  {this.property("Capital", capital)}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
+  property(label: string, value: string | number) {
+    return (
+      <p className="country-property">
+        {label}: <span className="label">{value}</span>
+      </p>
+    )
+  }
+}
---
+++ src/api.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosRequestConfig } from "axios"
+
+const API_ROOT = "https://restcountries.com/v2"
+
+interface TError {
+  status: number
+  message: string
+}
+
+export async function get<TResponse = any>(
+  url: string,
+  params: AxiosRequestConfig["params"] = {}
+): Promise<TResponse | undefined> {
+  try {
+    const response: any = await axios.get(`${API_ROOT}${url}`, {
+      params: { ...params }
+    })
+
+    if (!!response.data.status && response.data.status === 404) {
+      return
+    }
+
+    return response.data as TResponse
+  } catch (err) {
+    console.error(`Error with the API request -> ${err}`)
+  }
+}
